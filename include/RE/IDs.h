f5d926528b9278efdb1c09cb46b6b93f9d96b472
bob128730 <76489670+bob128730@users.noreply.github.com>
2026-07-21 09:52:59 +0100

    TESObjectREFR IDs

--- include/RE/IDs.h
+++ include/RE/IDs.h
@@ -2369,11 +2369,11 @@ namespace RE::ID
 		inline constexpr REL::ID ActivateRef{ 0 };          // 106374
 		inline constexpr REL::ID AddLockChange{ 0 };        // 106386
 		inline constexpr REL::ID GetCalcLevel{ 0 };         // 107531
-		inline constexpr REL::ID GetCurrentLocation{ 0 };   // 106554
+		inline constexpr REL::ID GetCurrentLocation{ 62697 };
 		inline constexpr REL::ID GetDistance{ 0 };          // 106562
 		inline constexpr REL::ID GetLinkedRef{ 0 };         // 107578
 		inline constexpr REL::ID GetLock{ 0 };              // 107581
-		inline constexpr REL::ID GetParentWorldSpace{ 0 };  // 106696
+		inline constexpr REL::ID GetParentWorldSpace{ 63417 };
 		inline constexpr REL::ID GetScale{ 0 };             // 106658
 		inline constexpr REL::ID GetSpaceship{ 119881 };
 		inline constexpr REL::ID GetSpaceshipParentDock{ 0 };  // 174134
@@ -2384,8 +2384,8 @@ namespace RE::ID
 		inline constexpr REL::ID IsInSpace{ 63482 };
 		inline constexpr REL::ID IsInSpaceship{ 119881 };
 		inline constexpr REL::ID IsObjectEquipped{ 0 };   // 106991
-		inline constexpr REL::ID IsSpaceshipDocked{ 0 };  // 174146
-		inline constexpr REL::ID IsSpaceshipLanded{ 0 };  // 173880
+		inline constexpr REL::ID IsSpaceshipDocked{ 120230 };
+		inline constexpr REL::ID IsSpaceshipLanded{ 119909 };
 		inline constexpr REL::ID WornHasKeyword{ 0 };     // 106992
 	}
 
